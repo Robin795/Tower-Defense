bef5bef00347bbc9acfceb358fc3736346042bd7
LeanderGitHub <leanderdiazbone@gmail.com>
2020-12-20 17:39:40 +0100

    started Project

---
+++ src/GameWindow.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+public class GameWindow extends JFrame{
+	private int width;
+	private int height;
+	
+	public GameWindow(int width, int height) {
+		super();
+		this.width = width;
+		this.height = height;
+		setSize(width,height);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	public void setVis(boolean visible) {
+		setVisible(visible);
+	}
+	@Override
+	public void paint(Graphics g) {
+		Color black = new Color(0, 0, 0);
+		g.setColor(black);
+		g.drawLine(0, (height/2)+20 , width, (height/2)+20);
+		g.drawLine(0 ,(height/2)-20 , width, (height/2)-20);
+	}
+}
---
+++ src/Tower.java
@@ -0,0 +1,14 @@
+
+public class Tower {
+	private int locationX;
+	private int locationY;
+	private String path;
+	public Tower(int locationX, int locationY) {
+		this.locationX = locationX;
+		this.locationY = locationY;
+	}
+	public void draw() {
+		
+	}
+	
+}
